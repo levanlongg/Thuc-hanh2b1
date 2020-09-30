a9b70a23dd918b8b4135adc4dd4bdb47f9205411
levanlongg <levanlong99pchy@gmail.com>
2020-09-30 22:33:30 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Bai1Component } from './bai1/bai1.component';
+import { MenuComponent } from './bai1/menu/menu.component';
+import { FooterComponent } from './bai1/footer/footer.component';
+import { MainComponent } from './bai1/main/main.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Bai1Component,
+    MenuComponent,
+    FooterComponent,
+    MainComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
